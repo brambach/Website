e3ca918f8fe0525409af6cbf2bf16d81a2828517
brambach <bryce.rambach@gmail.com>
2025-02-05 19:31:18 -0800

    starting to include mobile support

--- src/app/components/navigation/NavButton.jsx
+++ src/app/components/navigation/NavButton.jsx
@@ -1,6 +1,7 @@
 import { Github, Home, Linkedin, NotebookText, Palette, Phone, Twitter, User } from 'lucide-react';
 import Link from 'next/link'
 import React from 'react'
+import ResponsiveComponent from '../ResponsiveComponent';
 
 const getIcon = (icon) => {
     switch (icon) {
@@ -27,25 +28,55 @@ const getIcon = (icon) => {
 
 const NavButton = ({ x, y, label, link, icon, newTab }) => {
     return (
-        <div className='absolute z-20' 
-            style={{ transform: `translate(${x}, ${y})` }}
-        >
-            <Link 
-                href={link} 
-                target={newTab ? '_blank' : '_self'} 
-                className='text-foreground rounded-full flex items-center justify-center custom-bg'
-                aria-label={label} 
-                name={label}
-            >
-                <span className='relative peer w-14 h-14 p-4 animate-spin-slow-reverse group-hover:pause hover:text-accent'>
-                    {getIcon(icon)}
-                    <span className='peer bg-transparent absolute top-0 left-0 w-full h-full'/>
-                    <span className="absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap"> 
-                        {label}
-                    </span>
-                    </span>
-            </Link>
-        </div>
+        <ResponsiveComponent>
+
+            {({ size }) => {
+                return size && size >= 480 ?
+                    <div className='absolute z-20'
+                        style={{ transform: `translate(${x}, ${y})` }}
+                    >
+                        <Link
+                            href={link}
+                            target={newTab ? '_blank' : '_self'}
+                            className='text-foreground rounded-full flex items-center justify-center custom-bg'
+                            aria-label={label}
+                            name={label}
+                        >
+                            <span className='relative peer w-14 h-14 p-4 animate-spin-slow-reverse group-hover:pause hover:text-accent'>
+                                {getIcon(icon)}
+                                <span className='peer bg-transparent absolute top-0 left-0 w-full h-full' />
+                                <span className="absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap">
+                                    {label}
+                                </span>
+                            </span>
+                        </Link>
+                    </div>
+                    :
+
+                    <div className='w-fit z-20'
+                    >
+                        <Link
+                            href={link}
+                            target={newTab ? '_blank' : '_self'}
+                            className='text-foreground rounded-full flex items-center justify-center custom-bg'
+                            aria-label={label}
+                            name={label}
+                        >
+                            <span className='relative peer w-10 h-10 xs:w-14 xs:h-14 p-2.5 xs:p-4 hover:text-accent'>
+                                {getIcon(icon)}
+                                <span className='peer bg-transparent absolute top-0 left-0 w-full h-full' />
+                                <span className="absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap">
+                                    {label}
+                                </span>
+                            </span>
+                        </Link>
+                    </div>
+            }}
+
+
+
+
+        </ResponsiveComponent>
     )
 }
 
